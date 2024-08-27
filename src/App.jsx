3b9d9077b2b87e59fbbeedee9a0498ff41b86472
Talha-TN <nawazt964@gmail.com>
2024-08-27 11:34:07 -0700

    setup the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-
 import './App.css'
 function App() {
   return (
     <>
-      <h1>flowbite testing</h1>
+      <h1 className='text-2xl font-semibold text-center mt-10 tracking-[4px] text-blue-700'>flowbite testing</h1>
     </>
   )
 }
